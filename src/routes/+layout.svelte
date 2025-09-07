e23c529a042f96bc1f8b3f9338ceb74341b5e8f4
Sivayogeith Umamaheswaran <sivayogeith@gmail.com>
2025-09-07 10:36:34 +0530

    feat: added clicker, shop with some upgrades

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
-	
-	let { children } = $props();
+  import "../app.css";
+  import favicon from "$lib/assets/favicon.svg";
+
+  let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <link rel="icon" href={favicon} />
+  <style>
+    @import url("https://fonts.googleapis.com/css2?family=Comic+Relief:wght@400;700&display=swap");
+
+    body {
+      font-family: "Comic Relief", system-ui;
+      font-weight: 400;
+      font-style: normal;
+    }
+  </style>
 </svelte:head>
 
 {@render children?.()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,69 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  const upgrades = $state([
+    {
+      name: "2x",
+      description: "doubles click",
+      price: 100,
+      priceMultiplier: 3,
+      math: " * 2",
+      own: 0,
+    },
+    {
+      name: "x^2",
+      description: "squares click",
+      price: 1000,
+      priceMultiplier: 5,
+      math: " ^2",
+      own: 0,
+    },
+  ]);
+  let count = $state(0);
+  let equation = $state("1");
+
+  const onClick = () => {
+    count += eval(equation);
+  };
+
+  const buyUpgrade = (index: number) => {
+    const upgrade = upgrades[index];
+    if (upgrade.price <= count) {
+      count -= upgrade.price;
+      equation += upgrade.math;
+      upgrade.price *= upgrade.priceMultiplier;
+      upgrade.own++;
+    }
+  };
+</script>
+
+<div class="text-center">
+  <h1 class="text-3xl">math clicker</h1>
+</div>
+<div class="flex">
+  <div class="flex items-center md:w-[70%] w-[60%] flex-col">
+    <p class="flex justify-center items-center h-[10dvh] text-sky-700 text-lg">
+      equation: {equation}
+    </p>
+    <div class="flex justify-center items-center h-[60dvh] flex-col">
+      <p class="text-sky-700">balance: {count} m</p>
+      <button class="text-5xl" onclick={onClick}>meth</button>
+    </div>
+  </div>
+  <div class="flex h-[95dvh] md:w-[30%] w-[40%] rounded-l-2xl border p-5 flex-col">
+    <p class="text-3xl text-center mb-5">
+      shop <span class="text-sm">(click to buy)</span>
+    </p>
+    <ul class="lg:text-xl">
+      {#each upgrades as upgrade, i}
+        <div class="my-3">
+          <li class="flex justify-between">
+            <button onclick={() => buyUpgrade(i)}>
+              {upgrade.name} - {upgrade.description}
+            </button>
+            <p class="font-bold">{upgrade.price} m</p>
+          </li>
+          <p class="text-sm text-sky-700">you own {upgrade.own}</p>
+        </div>
+      {/each}
+    </ul>
+  </div>
+</div>
